45a03da76b2e256c8b5e7a1af1a5e9c1e6bbfd1a
pat-white-code <pat.k.white@gmail.com>
2020-09-03 12:34:08 -0500

    fixes pivotHelper

--- pivotHelper.js
+++ pivotHelper.js
@@ -1,12 +1,12 @@
 const swap = require('./swap');
 
-const pivotHelper = (arr, start = 0, end = arr.length) => {
+const pivotHelper = (arr, start = 0, end = arr.length - 1) => {
   // identify the first element in the arry
   // loop through the rest of the arry,
   // if pivot is greater than current item, increment pivot index, and the item at current index with item at current pivotIndex
   let pivot = arr[start];
-  let swapIndex = 0;
-  for(let i = start + 1 ; i < end; i++) {
+  let swapIndex = start;
+  for(let i = start + 1 ; i <= end; i++) {
     let current = arr[i];
     if(pivot > current) {
       swapIndex ++;
