76a9a1451510c37910ec627c38cf35ec09de6943
pat-white-code <pat.k.white@gmail.com>
2020-09-03 13:03:30 -0500

    adds stack overflow solution

--- getDigit.js
+++ getDigit.js
@@ -7,17 +7,24 @@
 // getDigit(75, 1) => 7
 // getDigit(441, 3) => 0
 
-// const getDigit = (num, pow) => {
-//   let divided = (num / Math.pow(10, pow));
-//   let floored = Math.floor(divided);
-//   let stringed = floored.toString();
-//   let digit = stringed[stringed.length - 1];
-//   return digit;
-// }
+const getDigit = (num, pow) => {
+  let divided = (num / Math.pow(10, pow));
+  let floored = Math.floor(divided);
+  let stringed = floored.toString();
+  let digit = stringed[stringed.length - 1];
+  return digit;
+}
 
 const getDigit = (num, pow) => {
   let str = Math.floor(num / Math.pow(10, pow)).toString();
   return str[str.length - 1];
+}
+
+// "Stack Overflow" Solution:
+const getDigit = (num, pow) => {
+  // Math.abs(num) means negative numbers will not distort the intended output.
+  // % 10 will always return the digit in the singles column in base 10 numbers.
+  return Math.floor(Math.abs(num) / Math.pow(10, pow)) % 10
 }
 
 console.log(getDigit(5644, 2));
