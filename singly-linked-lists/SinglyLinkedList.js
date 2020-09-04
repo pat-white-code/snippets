5436b14ec052eb25d9cae17e6433748f7a249046
pat-white-code <pat.k.white@gmail.com>
2020-09-04 12:03:21 -0500

    starts singly linked lists

---
+++ singly-linked-lists/SinglyLinkedList.js
@@ -0,0 +1,20 @@
+const ListNode = require('./ListNode');
+
+class SinglyLinkedList{
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val){
+    // add item to end of list
+    // create new node based on input val
+    // if there is no head, assign new node as head and as tail
+    // otherwise, point tail.next to new node, then assign this.tail as new node
+    
+  }
+}
+
+let newNode = new ListNode(55);
+
+console.log(newNode);
