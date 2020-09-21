96c8f30cbba9b8214d09f74cdb88da279c488140
pat-white-code <pat.k.white@gmail.com>
2020-09-21 10:02:36 -0500

    adds file for queues

---
+++ stacks-ques/queue.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(val) {
+    this.next = null;
+    this.val = val
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  enqueue(val) {
+    let newNode = new Node(val);
+    // add to the end of the list
+    if(!this.first) {
+      this.first = newNode;
+    }
+    this.last.next = newNode;
+    this.last = newNode
+  }
+}
