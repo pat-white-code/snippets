704ac54cdb2e529c66ecff92c8b9cf81e3421d15
pat-white-code <pat.k.white@gmail.com>
2020-09-23 10:41:45 -0500

    writes insert recursively

--- binary-search-trees/binary-search-tree.js
+++ binary-search-trees/binary-search-tree.js
@@ -11,31 +11,50 @@ class BST {
     this.root = null;
   }
 
-  insert(val) {
-    let newNode = new Node(val);
-    if(!this.root) {
-      this.root = newNode;
-    } else {
-      let current = this.root;
-      while (current) {
-        if (newNode.val > current.val){
+  // insert(val) {
+  //   let newNode = new Node(val);
+  //   if(!this.root) {
+  //     this.root = newNode;
+  //   } else {
+  //     let current = this.root;
+  //     while (current) {
+  //       if (newNode.val > current.val){
 
-          if(current.right) {
-            current = current.right;
-          } else {
-            current.right = newNode;
-            break;
-          }
-        } else {
-          if(current.left) {
-            current = current.left;
-          } else {
-            current.left = newNode;
-            break;
-          }
-        }
-      }
+  //         if(current.right) {
+  //           current = current.right;
+  //         } else {
+  //           current.right = newNode;
+  //           break;
+  //         }
+  //       } else {
+  //         if(current.left) {
+  //           current = current.left;
+  //         } else {
+  //           current.left = newNode;
+  //           break;
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  insert (val, current = this.root) {
+    // base case:
+    if (!this.root) {
+      this.root = new Node(val);
+      return true;
     }
+    if (val > current.val && !current.right) {
+      current.right = new Node(val);
+      return true;
+    }
+    if( val < current.val && !current.left) {
+      current.left = new Node (val);
+      return true;
+    }
+    (val > current.val) ? 
+      this.insert(val, current.right) : 
+      this.insert(val, current.left);
   }
 }
 
