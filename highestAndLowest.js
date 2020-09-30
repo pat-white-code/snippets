a2e847ff84e27d19bc9a83882a55b9ed4dcd13dc
pat-white-code <pat.k.white@gmail.com>
2020-09-30 10:19:01 -0500

    writes highest lowest

--- highestAndLowest.js
+++ highestAndLowest.js
@@ -9,12 +9,24 @@
 
 const highAndLow = (str) => {
   let ints = str.split(' ').map(num => parseInt(num));
-  console.log(ints);
   let greatest = Math.max(...ints).toString();
   let smallest = Math.min(...ints).toString();
-
   return `${greatest} ${smallest}`
 };
+
+const highAndLow = (str) => {
+  let greatest = str[0];
+  let smallest = str[0];
+  for(let i = 0 ; i < str.length ; i++) {
+    let num = str[i]
+    if(parseInt(str[i])) {
+      // console.log(num);
+      greatest = Math.max(parseInt(num), parseInt(greatest)).toString();
+      smallest = Math.min(parseInt(num), parseInt(smallest)).toString();
+    }
+  }
+  return greatest + ' ' + smallest
+}
 
 console.log(highAndLow("1 2 3 4 5"))
 console.log(highAndLow("1 2 -3 4 5"))
