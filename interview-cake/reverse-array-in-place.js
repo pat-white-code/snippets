699bce57aaa42d83ff00433f60f6ea9e6458860e
pat-white-code <pat.k.white@gmail.com>
2020-10-20 12:34:25 -0500

    does interview cake reverse array in place

--- interview-cake/reverse-array-in-place.js
+++ interview-cake/reverse-array-in-place.js
@@ -5,10 +5,19 @@ const reverse = (arr) => {
   // [c, a, t] => [t, a, c]
   // swapping in place means we can't simply create an empty array and iterate the array backwards to build a new array
   // instead we have to swap items in the original array
-  // i                  j
+  //          i                   
   // [p a t c h a d a m s]
+  //              j
   // while i < j
   // swap the elements at arr[i] and arr[j]
-  let i = 0, j = arr.length - 1;
-  
+  let i = 0;
+  let j = arr.length - 1;
+  while(i < j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]]
+    i++
+    j--
+  }
+  return arr;
 }
+
+module.exports = reverse;
--- test/reverse-array-in-place.js
+++ test/reverse-array-in-place.js
@@ -1,10 +1,11 @@
 const chai = require('chai'), expect = chai.expect;
+const reverse = require('../interview-cake/reverse-array-in-place');
 
 
 describe('reverse-in-place', ()=> {
   it('should reverse the array', ()=> {
     let returned = reverse(['p', 'a', 't', 'r', 'i', 'c', 'k'])
 
-    expect(returned).to.deep.equal('k', 'c', 'i', 'r', 't', 'a', 'p')
+    expect(returned).to.deep.equal(['k', 'c', 'i', 'r', 't', 'a', 'p'])
   })
 })
