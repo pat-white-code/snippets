69e2953309a63b60619a6b1d5d281b914b7f964e
pat-white-code <pat.k.white@gmail.com>
2020-11-16 11:29:30 -0600

    adds example solution

--- interview-cake/permutationPalindrome.js
+++ interview-cake/permutationPalindrome.js
@@ -22,3 +22,22 @@ const permutationPalindrome = (str) => {
 module.exports = {
   permutationPalindrome
 }
+
+
+// function hasPalindromePermutation(theString) {
+
+//   // Track characters we've seen an odd number of times
+//   const unpairedCharacters = new Set();
+
+//   for (let char of theString) {
+//     if (unpairedCharacters.has(char)) {
+//       unpairedCharacters.delete(char);
+//     } else {
+//       unpairedCharacters.add(char);
+//     }
+//   }
+
+//   // The string has a palindrome permutation if it
+//   // has one or zero characters without a pair
+//   return unpairedCharacters.size <= 1;
+// }
