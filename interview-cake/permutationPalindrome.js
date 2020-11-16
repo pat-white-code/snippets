1c1c21d7dc5711ceea8d97bcb95c95e7173576f1
pat-white-code <pat.k.white@gmail.com>
2020-11-16 11:21:43 -0600

    writes permutationsPalindrome

---
+++ interview-cake/permutationPalindrome.js
@@ -0,0 +1,20 @@
+const permutationPalindrome = (str) => {
+  let obj = {};
+  let odds = 0;
+  for(let i = 0 ; i < str.length ; i++) {
+    let letter = str[i];
+    if(obj[letter]) { 
+      obj[letter]++;
+      if(obj[letter] % 2 === 0) {
+        odds--
+      } else {
+        odds++
+      }
+    } else {
+      obj[letter] = 1;
+      odds++
+    }
+  }
+  if(odds > 1) return false;
+  return true;
+}
